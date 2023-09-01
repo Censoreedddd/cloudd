c9878a12ea6aa1191891f93ea3c89e51f7bd2339
Censoreedddd <azizchikhi685@gmail.com>
2023-09-01 15:52:38 +0100

    go

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Map from './Map';
+import StyledMapContainer from './StyledMapContainer';
+
+function App() {
+  return (
+    <StyledMapContainer>
+      <Map />
+    </StyledMapContainer>
+  );
+}
+
+export default App;
